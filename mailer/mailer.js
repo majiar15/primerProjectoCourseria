c681724944a43aef568f166385fb499f494f2a32
martin jimenez <majiar16@gmail.com>
2020-07-28 10:13:08 -0500

    prueba 4

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -2,42 +2,37 @@ const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 
 let mailConfig;
-// if(process.env.NODE_ENV === "production"){
-//     const option ={
-//         auth:{
-//             api_key: process.env.SENDGRID_API_KEY
-//         }
-//     }
-//     mailConfig = sgTransport(option);
-// }else{
-//     if(process.env.NODE_ENV === "starting"){
-//         console.log('XXXXXXXXXXXX');
-//         const option = {
-//             auth:{
-//                 api_key: process.env.SENDGRID_API_KEY
-//             }
-//         }
-//         mailConfig = sgTransport(option);
-//     }else{
-//         //all main send ethereal 
-        
-//         mailerConfig = {
-//             host: 'smtp.ethereal.email',
-//             port: 587,
-//             auth: {
-//                 user: process.env.ETHEREAL_USER,
-//                 pass: process.env.ETHEREAL_PASSWORD
-//             }
-//         }
-//     }
-
-// }
-const option ={
-    auth:{
-        api_key: process.env.SENDGRID_API_KEY
+if(process.env.NODE_ENV === "production"){
+    const option ={
+        auth:{
+            api_key: process.env.SENDGRID_API_SECRECT
+        }
     }
+    mailConfig = sgTransport(option);
+}else{
+    if(process.env.NODE_ENV === "staging"){
+        console.log('XXXXXXXXXXXX');
+        const option = {
+            auth:{
+                api_key: process.env.SENDGRID_API_SECRECT
+            }
+        }
+        mailConfig = sgTransport(option);
+    }else{
+        //all main send ethereal 
+        
+        mailerConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_USER,
+                pass: process.env.ETHEREAL_PASSWORD
+            }
+        }
+    }
+
 }
-mailConfig = sgTransport(option);
+
 
 
 module.exports = nodemailer.createTransport(mailerConfig);
