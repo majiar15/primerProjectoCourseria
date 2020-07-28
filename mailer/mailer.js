44c890668447e1e76c602630f3558b0be3aac34f
martin jimenez <majiar16@gmail.com>
2020-07-28 09:26:22 -0500

    prueba

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -2,37 +2,44 @@ const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 
 let mailConfig;
-if(process.env.NODE_ENV === "production"){
-    const option ={
-        auth:{
-            api_key: process.env.SENDGRID_API_KEY
-        }
-    }
-    mailConfig = sgTransport(option);
-}else{
-    if(process.env.NODE_ENV === "starting"){
-        console.log('XXXXXXXXXXXX');
-        const option = {
-            auth:{
-                api_key: process.env.SENDGRID_API_KEY
-            }
-        }
-        mailConfig = sgTransport(option);
-    }else{
-        //all main send ethereal 
+// if(process.env.NODE_ENV === "production"){
+//     const option ={
+//         auth:{
+//             api_key: process.env.SENDGRID_API_KEY
+//         }
+//     }
+//     mailConfig = sgTransport(option);
+// }else{
+//     if(process.env.NODE_ENV === "starting"){
+//         console.log('XXXXXXXXXXXX');
+//         const option = {
+//             auth:{
+//                 api_key: process.env.SENDGRID_API_KEY
+//             }
+//         }
+//         mailConfig = sgTransport(option);
+//     }else{
+//         //all main send ethereal 
         
-        mailerConfig = {
-            host: 'smtp.ethereal.email',
-            port: 587,
-            auth: {
-                user: process.env.ETHEREAL_USER,
-                pass: process.env.ETHEREAL_PASSWORD
-            }
-        }
+//         mailerConfig = {
+//             host: 'smtp.ethereal.email',
+//             port: 587,
+//             auth: {
+//                 user: process.env.ETHEREAL_USER,
+//                 pass: process.env.ETHEREAL_PASSWORD
+//             }
+//         }
+//     }
+
+// }
+
+mailerConfig = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: process.env.ETHEREAL_USER,
+        pass: process.env.ETHEREAL_PASSWORD
     }
-
 }
-
-
 
 module.exports = nodemailer.createTransport(mailerConfig);
