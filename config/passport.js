1b645d2a073935593a1c4504c9478ca94e02a75d
martin jimenez <majiar16@gmail.com>
2020-07-28 16:22:47 -0500

    enlace login with google

--- config/passport.js
+++ config/passport.js
@@ -19,7 +19,7 @@ Passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
+    callbackURL: process.env.HOST+"/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
        Usuario.findOrCreate(profile, function (err, user) {
